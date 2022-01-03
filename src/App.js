bc17e2908bfd7230f67531bc2cb30dff69fec45a
Zainab Omar <zainab.alshaikhli01@gmail.com>
2022-01-03 14:07:42 -0600

    create components folder and Signup component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import Signup from './components/Signup'
 
 function App() {
   return (
     <div className="App">
       Welcome to fitness app
+      <Signup />
  
     </div>
   );
