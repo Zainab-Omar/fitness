4d985cf0846df64b2b0238bf8553de01d5cff3f5
Zainab Omar <zainab.alshaikhli01@gmail.com>
2022-01-26 15:48:56 -0600

    configure redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
+import thunk from 'redux-thunk';
+import {createStore, applyMiddleware, compose} from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/manageReducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
