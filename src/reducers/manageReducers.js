d88ad786cb66aec7d64a608d35e7a0acb7eddc86
Zainab Omar <zainab.alshaikhli01@gmail.com>
2022-01-26 20:01:00 -0600

    create LOGIN_USER and ERROR_MESSAGE reducers

--- src/reducers/manageReducers.js
+++ src/reducers/manageReducers.js
@@ -1,10 +1,22 @@
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
- 
-    
+    users: userReducer   
 })
 
 export default rootReducer;
+
+function userReducer(state = [], action){
+    switch(action.type){
+        case 'LOGIN_USER':
+          return {...state, currentUser: action.userObj}
+
+          case 'ERROR_MESSAGE':
+            return {...state, error: action.error}
+
+            default:
+             return state;
+    }
+}
 
 
