23d14db63687560d9bf7988a4f6664d77a3b3924
Zainab Omar <zainab.alshaikhli01@gmail.com>
2022-01-27 22:50:02 -0600

    display data on screen

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Card, Button } from 'react-bootstrap'
 
 const Exercise =({exercise}) => {
     return(
         <div>
-            my exercises
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={exercise.gifUrl} />
+                <Card.Body>
+                    <Card.Title>{exercise.name}</Card.Title>
+                    <Card.Text>
+                    <p>Body Part: {exercise.bodyPart}</p>
+                    <p>Equipment: {exercise.equipment}</p>
+                    <p>Target: {exercise.target}</p>
+                    </Card.Text>
+                    <Button variant="primary">Add</Button>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
