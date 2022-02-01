469a77252757fd7a13b078f70dd7a8a1fa7d0cbc
Zainab Omar <zainab.alshaikhli01@gmail.com>
2022-02-01 16:55:25 -0600

    add useNavigate to logout component

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,13 +2,17 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { logout } from '../actions/logout'
 import { Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 function Logout({logout}){
+
+    const navigate = useNavigate()
 
     const handleClick = e => {
         e.preventDefault()
         localStorage.removeItem("token")
         logout()
+        navigate("./login")
     }
     return(
         <Button className="logout-button" variant="link" onClick={handleClick}>Logout</Button>
