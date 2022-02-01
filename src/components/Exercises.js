391f889d28f0707721063192b5f059aff38f997d
Zainab Omar <zainab.alshaikhli01@gmail.com>
2022-02-01 17:23:21 -0600

    add uuidv4 to my json package and import it in Exercises component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Exercise from './Exercise'
+import { v4 as uuidv4 } from 'uuid';
 
 const Exercises = ({execises}) => {
     // in case of receiving and empty array return [] so map method does not throw error
 
     const execisesArray = execises || []
-    const renderexecises = execisesArray.map(exercise => <Exercise exercise={exercise}/>)
+    const renderexecises = execisesArray.map(exercise => <Exercise key={uuidv4()} exercise={exercise}/>)
 
     return(
         <div>
