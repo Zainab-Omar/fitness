3364d0e0efd6d91547ba3b2a2b6d5008bb71d3fb
Zainab Omar <zainab.alshaikhli01@gmail.com>
2022-02-07 12:23:41 -0600

    add useNavigate to Profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,24 @@
 import React from "react";
-// import { profile } from '../actions/profile'
 import { connect } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 function Profile({users}) {
 
-    return(
-        <div>
-            Profile
-        </div>
-    )
+    const navigate = useNavigate();
 
+    if(localStorage.token !== undefined){
+        if(users !== undefined){
+            return(
+                <h1>Welcome {users.data.attributes.user_name}</h1>
+            )
+        }
+        else return null    
+    }
+    else{
+        return(
+            navigate('/login')
+        )
+    }
 }
 
 const mapStateToProps = state => {
