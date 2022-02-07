ad42055a1d4671e0b4880ede5ea606b6a0f8a95d
Zainab Omar <zainab.alshaikhli01@gmail.com>
2022-02-07 13:28:07 -0600

    add three picture to my profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,7 +9,23 @@ function Profile({users}) {
     if(localStorage.token !== undefined){
         if(users !== undefined){
             return(
-                <h1>Welcome {users.data.attributes.user_name}</h1>
+                <React.Fragment>
+                    <h1>Welcome {users.data.attributes.user_name}</h1>
+
+                    <figure>
+                        <img src="/images/workout.jpg" alt="workout" height={250} width={300}/>
+                        <figcaption>My Exercises</figcaption>
+                    </figure>
+                    <figure>
+                        <img src="/images/bmi.jpeg" alt="body mass index"height={250} width={300}/>
+                        <figcaption>Calculate your BMI</figcaption>
+                    </figure>
+                    <figure>
+                        <img src="/images/health-food.jpeg" alt="health food" height={250} width={300}/>
+                        <figcaption>Find Healthy Recipes</figcaption>
+                    </figure>      
+                    
+                </React.Fragment>
             )
         }
         else return null    
