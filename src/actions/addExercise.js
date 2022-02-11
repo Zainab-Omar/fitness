b678a77ae375645de1d24f03af4b3e98510df9e1
Zainab Omar <zainab.alshaikhli01@gmail.com>
2022-02-11 13:51:05 -0600

    create addExercise action

--- src/actions/addExercise.js
+++ src/actions/addExercise.js
@@ -1,18 +1,18 @@
-export const addBMI = (id, bmi) => {
-    return (dispatch) => {
-        fetch(`http://localhost:3000/api/v1/users/${id}/bmis`, {
+export const addExercise = (id, exercise) => {
+    return(dispatch)=> {
+        fetch(`http://localhost:3000/api/v1/users/${id}/exercises`,
+        {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
                 "Accept": "application/json"
             },
-            body: JSON.stringify({bmi})
+            body: JSON.stringify({exercise})
         })
         .then(res => res.json())
         .then(data => console.log(data))
         .catch(err => {
             console.error(err);
-        });
+        })
     }
 }
-
